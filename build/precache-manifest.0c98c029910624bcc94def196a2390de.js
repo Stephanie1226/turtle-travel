b6d05374f907966741c8dcaf5c59c22349f7d3d9
Chieh Wang <chiehwang1226@gmail.com>
2020-05-17 15:37:13 -0600

    build file

---
+++ build/precache-manifest.0c98c029910624bcc94def196a2390de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ac02294ceb887ddb0c1aadc1521c91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388ad4b6fbc9f9d4ab99",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3228e89677032873264d",
+    "url": "/static/css/main.25e4901a.chunk.css"
+  },
+  {
+    "revision": "388ad4b6fbc9f9d4ab99",
+    "url": "/static/js/2.a3ae0f54.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a3ae0f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3228e89677032873264d",
+    "url": "/static/js/main.84b1012c.chunk.js"
+  },
+  {
+    "revision": "651ed5553a11530c095c",
+    "url": "/static/js/runtime-main.47ba07b8.js"
+  }
+]);
